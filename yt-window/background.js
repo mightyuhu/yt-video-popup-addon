01064c4efd90b5736ef8691996c94db2e6267a60
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2017-12-11 19:44:13 +0100

    update manifest and api

--- yt-window/background.js
+++ yt-window/background.js
@@ -6,11 +6,11 @@ function start(tab) {
   chrome.extension.getBackgroundPage().console.log(tab);
   var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
   var match = tab.url.match(regExp);
-  if (match&&match[2].length==11){
+  if (match && match[2].length == 11){
       chrome.windows.create(
          {
-          url:'https://youtube.com/watch_popup?v='+match[2],
-          type:'detached_panel'
+          url:'https://www.youtube.com/embed/'+match[2]+'?showinfo=0&rel=0&modestbranding=1&autoplay=1',
+          type:'popup'
          },
          function(){});
   }
